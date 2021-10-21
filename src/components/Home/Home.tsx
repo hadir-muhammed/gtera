757faa155b50c0530c973b3e8a323fa2593093ad
Hadeer Mohamed <hadeer.mohamed@incorta.com>
2021-10-21 07:49:16 +0200

    edit in design

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -4,6 +4,12 @@ import { ReactComponent as GteraLogo } from "../../images/Gtera-Logo.svg";
 import { ReactComponent as Sandwatch } from "../../images/sandwatch.svg";
 import { ReactComponent as Message } from "../../images/message.svg";
 import { ReactComponent as Group } from "../../images/Group.svg";
+import { ReactComponent as CallIcon } from "../../images/call_black.svg";
+import { ReactComponent as LocationIcon } from "../../images/fmd_good.svg";
+import { ReactComponent as LinkedIn } from "../../images/linkedin.svg";
+import { ReactComponent as Twitter } from "../../images/twitter.svg";
+import { ReactComponent as Facebook } from "../../images/facebook.svg";
+
 import ContactUs from "../ContactUs/ContactUs";
 import "./Home.css";
 
@@ -58,10 +64,53 @@ export default function Home() {
         </div>
       </div>
       <div className="footer">
-        <Group />
-        <span className="footer-text">
-          All rights reserved to Gtera Solutions Company.2021
-        </span>
+        <div>
+          <div className="footer-info">
+            <div>
+              <CallIcon />
+            </div>
+            <div>
+              <span className="desktop">
+                Egypt: +20 1033 033 411 / Qatar: +974 3000 1629
+              </span>
+              <p className="mobile">Egypt: +20 1033 033 411</p>
+              <p className="mobile">Qatar: +974 3000 1629</p>
+            </div>
+          </div>
+          <div className="footer-info">
+            <div>
+              <LocationIcon />
+            </div>
+            <div>
+              <p className="egypt-location">
+                Egypt: 20 Greentowers Compound, 20 St., Smouha, Alexandria.
+              </p>
+              <p className="qatar-location">
+                Qatar: Building 34, 319 St., Zone 69, Marina Twin Towers,
+                Lusail.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div className="social-media-icons">
+            <a href="ggoge.com" target="_blank">
+              <LinkedIn />
+            </a>
+            <a href="google" target="_blank">
+              <Facebook />
+            </a>
+            <a href="google" target="_blank">
+              <Twitter />
+            </a>
+          </div>
+          <div className="footer-rights-container">
+            <Group />
+            <span className="footer-rights">
+              All rights reserved to Gtera Solutions Company.2021
+            </span>
+          </div>
+        </div>
       </div>
       <ContactUs
         drawerIsShown={showContactUs}

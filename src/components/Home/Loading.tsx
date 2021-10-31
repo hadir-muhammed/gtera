d4da6b0ad1e513ceb118bd5e22d11bdbd0ba4104
Moustafa Ahmed <eng.moustafa.it@gmail.com>
2021-10-31 08:54:32 +0300

    reset monitor ui element posiitons

--- src/components/Home/Loading.tsx
+++ src/components/Home/Loading.tsx
@@ -25,22 +25,24 @@ const Loading: FC<Props> = (props: Props) => {
                             Stay Tuned.
                         </p>
                         <span className="arrow">{`/>`}</span>
+
+                        <Button
+                            onClick={() => props.setShowContactUs(true)}
+                            className="contact-us-btn"
+                        >
+                            <img src={Message} />
+                            <span className="btn-text"> Contact Us</span>
+                        </Button>
                     </h4>
-                    <Button
-                        onClick={() => props.setShowContactUs(true)}
-                        className="contact-us-btn"
-                    >
-                        <img src={Message} />
-                        <span className="btn-text"> Contact Us</span>
-                    </Button>
+
+                    <Progress
+                        strokeColor={{
+                            "0%": "#384B5E",
+                            "100%": "#EC5F60",
+                        }}
+                        percent={20}
+                    />
                 </div>
-                <Progress
-                    strokeColor={{
-                        "0%": "#384B5E",
-                        "100%": "#EC5F60",
-                    }}
-                    percent={20}
-                />
             </div>
             {/* <div className="sandwatch-icon">
                 <img src={Sandwatch} />
